d6565bdf4726ec32c329291fe7ad66ec3267480c
Remi Caput <git@mirari.fr>
2020-08-02 20:00:50 +0200

    Apply automatic code formatting rules.
    * PHP => PSR-2
    * JS => VS Code defaults

--- src/index.php
+++ src/index.php
@@ -5,15 +5,15 @@
 */
 class State
 {
-	public $cloudiness;
-	public $condition;
-	public $day;
-	public $hour;
-	public $minute;
-	public $month;
-	public $temperature;
-	public $user;
-	public $weather;
+    public $cloudiness;
+    public $condition;
+    public $day;
+    public $hour;
+    public $minute;
+    public $month;
+    public $temperature;
+    public $user;
+    public $weather;
 };
 
 /*
@@ -22,24 +22,24 @@ class State
 ** $url:	target URL
 ** return:	JSON response
 */
-function http_get_json ($agent, $url)
+function http_get_json($agent, $url)
 {
-	$handle = curl_init ();
+    $handle = curl_init();
 
-	curl_setopt ($handle, CURLOPT_CONNECTTIMEOUT_MS, 5000);
-	curl_setopt ($handle, CURLOPT_TIMEOUT_MS, 1000);
-	curl_setopt ($handle, CURLOPT_USERAGENT, $agent);
-	curl_setopt ($handle, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt ($handle, CURLOPT_SSL_VERIFYHOST, false);
-	curl_setopt ($handle, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt ($handle, CURLOPT_TCP_NODELAY, false);
-	curl_setopt ($handle, CURLOPT_URL, $url);
+    curl_setopt($handle, CURLOPT_CONNECTTIMEOUT_MS, 5000);
+    curl_setopt($handle, CURLOPT_TIMEOUT_MS, 1000);
+    curl_setopt($handle, CURLOPT_USERAGENT, $agent);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($handle, CURLOPT_TCP_NODELAY, false);
+    curl_setopt($handle, CURLOPT_URL, $url);
 
-	$output = curl_exec ($handle);
+    $output = curl_exec($handle);
 
-	curl_close ($handle);
+    curl_close($handle);
 
-	return $output !== false ? json_decode ($output, true) : null;
+    return $output !== false ? json_decode($output, true) : null;
 }
 
 /*
@@ -51,82 +51,154 @@ function http_get_json ($agent, $url)
 ** $user:	current user id or 0 for guest users
 ** return:	selected variant name
 */
-function get_variant ($agent, $appid, $time, $user)
+function get_variant($agent, $appid, $time, $user)
 {
-	// Skip variant selection entirely for guest users. This is required to
-	// save most requests to OpenWeatherMap and FreeGeoIP as they're limited.
-	if ($user !== 0)
-	{
-		// Get current day and time
-		list ($month, $day, $hour, $minute) = explode ('-', date ('m-d-H-i', $time));
+    // Skip variant selection entirely for guest users. This is required to
+    // save most requests to OpenWeatherMap and FreeGeoIP as they're limited.
+    if ($user !== 0) {
+        // Get current day and time
+        list($month, $day, $hour, $minute) = explode('-', date('m-d-H-i', $time));
 
-		// Get weather from OpenWeatherMap API
-		$weather = get_weather ($agent, $appid);
+        // Get weather from OpenWeatherMap API
+        $weather = get_weather($agent, $appid);
 
-		// Merge datetime and weather into single state structure
-		$state = new State ();
-		$state->cloudiness	= isset ($weather['clouds']['all']) ? (int)$weather['clouds']['all'] : 0; // Cloudiness %
-		$state->condition	= isset ($weather['weather'][0]['main']) ? strtolower ($weather['weather'][0]['main']) : ''; // Condition name
-		$state->day			= (int)$day;
-		$state->hour		= (int)$hour;
-		$state->minute		= (int)$minute;
-		$state->month		= (int)$month;
-		$state->temperature	= isset ($weather['main']['temp']) ? (int)$weather['main']['temp'] : 293.15; // Temperature in Kelvins
-		$state->user		= $user;
-		$state->weather		= isset ($weather['weather'][0]['id']) ? (int)$weather['weather'][0]['id'] : 0; // Weather identifier
+        // Merge datetime and weather into single state structure
+        $state = new State();
+        $state->cloudiness	= isset($weather['clouds']['all']) ? (int)$weather['clouds']['all'] : 0; // Cloudiness %
+        $state->condition	= isset($weather['weather'][0]['main']) ? strtolower($weather['weather'][0]['main']) : ''; // Condition name
+        $state->day			= (int)$day;
+        $state->hour		= (int)$hour;
+        $state->minute		= (int)$minute;
+        $state->month		= (int)$month;
+        $state->temperature	= isset($weather['main']['temp']) ? (int)$weather['main']['temp'] : 293.15; // Temperature in Kelvins
+        $state->user		= $user;
+        $state->weather		= isset($weather['weather'][0]['id']) ? (int)$weather['weather'][0]['id'] : 0; // Weather identifier
 
-		// Select boo variant according to current state
-		$matches = array
-		(
-			'drunk'				=> function ($state) { return $state->month === 1 && $state->day === 1; },
-			'crown'				=> function ($state) { return $state->month === 1 && $state->day === 6; },
-			'yoshi'				=> function ($state) { return $state->month === 1 && $state->day === 24; },
-			'pancake'			=> function ($state) { return $state->month === 2 && $state->day === 2; },
-			'valentine'			=> function ($state) { return $state->month === 2 && $state->day === 14; },
-			'cardboard'			=> function ($state) { return $state->month === 2 && $state->day === 17; },
-			'bear'				=> function ($state) { return $state->month === 6 && $state->day === 25; },
-			'redhat'			=> function ($state) { return $state->month === 7 && $state->day === 1; },
-			'fireworks'			=> function ($state) { return $state->month === 7 && $state->day === 14; },
-			'snake'				=> function ($state) { return $state->month === 8 && $state->day === 2; },
-			'china'				=> function ($state) { return $state->month === 8 && $state->day === 29; },
-			'unicorn'			=> function ($state) { return $state->month === 9 && $state->day === 9; },
-			'penguin'			=> function ($state) { return $state->month === 9 && $state->day === 12; },
-			'space'				=> function ($state) { return $state->month === 9 && $state->day === 27; },
-			'particule'			=> function ($state) { return $state->month === 10 && $state->day === 15; },
-			'swan'				=> function ($state) { return $state->month === 10 && $state->day === 18; },
-			'pizza'				=> function ($state) { return $state->month === 10 && $state->day === 21; },
-			'pixel'				=> function ($state) { return $state->month === 10 && $state->day === 23; },
-			'ghost'				=> function ($state) { return $state->month === 10 && $state->day === 31; },
-			'totoro'			=> function ($state) { return $state->month === 11 && $state->day === 4; },
-			'cornflower'		=> function ($state) { return $state->month === 11 && $state->day === 11; },
-			'kirby'				=> function ($state) { return $state->month === 12 && $state->day === 19; },
-			'christmas-dark'	=> function ($state) { return $state->month === 12 && $state->day < 24 && ($state->hour + 2) % 24 <= 7; },
-			'christmas'			=> function ($state) { return $state->month === 12 && $state->day < 24; },
-			'santa'				=> function ($state) { return $state->month === 12 && $state->day === 24; },
-			'gift'				=> function ($state) { return $state->month === 12 && $state->day === 25; },
-			'cola'				=> function ($state) { return $state->month === 12 && $state->day === 28; },
-			'yellow'			=> function ($state) { return $state->month === 12 && $state->day < 31; },
-			'party'				=> function ($state) { return $state->month === 12 && $state->day === 31; },
-			'dark'				=> function ($state) { return $state->hour === 3 && $state->minute === 0; },
-			'sleep'				=> function ($state) { return ($state->hour + 1) % 24 <= 7; },
-			'freeze'			=> function ($state) { return $state->temperature < 269.15; },
-			'bobblehat'			=> function ($state) { return $state->temperature < 277.15; },
-			'hot'				=> function ($state) { return $state->temperature > 305.15; },
-			'umbrella'			=> function ($state) { return $state->condition === 'rain' && $state->weather > 500 && $state->cloudiness >= 50; },
-			'snow'				=> function ($state) { return $state->condition === 'snow' && $state->weather > 600 && $state->cloudiness >= 50; },
-			'sunglass'			=> function ($state) { return $state->hour >= 10 && $state->hour <= 18 && $state->cloudiness < 10 && $state->temperature > 297.15; }
-		);
+        // Select boo variant according to current state
+        $matches = array(
+            'drunk'				=> function ($state) {
+                return $state->month === 1 && $state->day === 1;
+            },
+            'crown'				=> function ($state) {
+                return $state->month === 1 && $state->day === 6;
+            },
+            'yoshi'				=> function ($state) {
+                return $state->month === 1 && $state->day === 24;
+            },
+            'pancake'			=> function ($state) {
+                return $state->month === 2 && $state->day === 2;
+            },
+            'valentine'			=> function ($state) {
+                return $state->month === 2 && $state->day === 14;
+            },
+            'cardboard'			=> function ($state) {
+                return $state->month === 2 && $state->day === 17;
+            },
+            'bear'				=> function ($state) {
+                return $state->month === 6 && $state->day === 25;
+            },
+            'redhat'			=> function ($state) {
+                return $state->month === 7 && $state->day === 1;
+            },
+            'fireworks'			=> function ($state) {
+                return $state->month === 7 && $state->day === 14;
+            },
+            'snake'				=> function ($state) {
+                return $state->month === 8 && $state->day === 2;
+            },
+            'china'				=> function ($state) {
+                return $state->month === 8 && $state->day === 29;
+            },
+            'unicorn'			=> function ($state) {
+                return $state->month === 9 && $state->day === 9;
+            },
+            'penguin'			=> function ($state) {
+                return $state->month === 9 && $state->day === 12;
+            },
+            'space'				=> function ($state) {
+                return $state->month === 9 && $state->day === 27;
+            },
+            'particule'			=> function ($state) {
+                return $state->month === 10 && $state->day === 15;
+            },
+            'swan'				=> function ($state) {
+                return $state->month === 10 && $state->day === 18;
+            },
+            'pizza'				=> function ($state) {
+                return $state->month === 10 && $state->day === 21;
+            },
+            'pixel'				=> function ($state) {
+                return $state->month === 10 && $state->day === 23;
+            },
+            'ghost'				=> function ($state) {
+                return $state->month === 10 && $state->day === 31;
+            },
+            'totoro'			=> function ($state) {
+                return $state->month === 11 && $state->day === 4;
+            },
+            'cornflower'		=> function ($state) {
+                return $state->month === 11 && $state->day === 11;
+            },
+            'kirby'				=> function ($state) {
+                return $state->month === 12 && $state->day === 19;
+            },
+            'christmas-dark'	=> function ($state) {
+                return $state->month === 12 && $state->day < 24 && ($state->hour + 2) % 24 <= 7;
+            },
+            'christmas'			=> function ($state) {
+                return $state->month === 12 && $state->day < 24;
+            },
+            'santa'				=> function ($state) {
+                return $state->month === 12 && $state->day === 24;
+            },
+            'gift'				=> function ($state) {
+                return $state->month === 12 && $state->day === 25;
+            },
+            'cola'				=> function ($state) {
+                return $state->month === 12 && $state->day === 28;
+            },
+            'yellow'			=> function ($state) {
+                return $state->month === 12 && $state->day < 31;
+            },
+            'party'				=> function ($state) {
+                return $state->month === 12 && $state->day === 31;
+            },
+            'dark'				=> function ($state) {
+                return $state->hour === 3 && $state->minute === 0;
+            },
+            'sleep'				=> function ($state) {
+                return ($state->hour + 1) % 24 <= 7;
+            },
+            'freeze'			=> function ($state) {
+                return $state->temperature < 269.15;
+            },
+            'bobblehat'			=> function ($state) {
+                return $state->temperature < 277.15;
+            },
+            'hot'				=> function ($state) {
+                return $state->temperature > 305.15;
+            },
+            'umbrella'			=> function ($state) {
+                return $state->condition === 'rain' && $state->weather > 500 && $state->cloudiness >= 50;
+            },
+            'snow'				=> function ($state) {
+                return $state->condition === 'snow' && $state->weather > 600 && $state->cloudiness >= 50;
+            },
+            'sunglass'			=> function ($state) {
+                return $state->hour >= 10 && $state->hour <= 18 && $state->cloudiness < 10 && $state->temperature > 297.15;
+            }
+        );
 
-		// Search for matching predicate and return associated variant name
-		foreach ($matches as $name => $predicate)
-		{
-			if ($predicate ($state))
-				return $name;
-		}
-	}
+        // Search for matching predicate and return associated variant name
+        foreach ($matches as $name => $predicate) {
+            if ($predicate($state)) {
+                return $name;
+            }
+        }
+    }
 
-	// No match, fallback to default variant name
-	return 'default';
+    // No match, fallback to default variant name
+    return 'default';
 }
 
 /*
@@ -136,27 +208,29 @@ function get_variant ($agent, $appid, $time, $user)
 ** $appid:	application identifier for OpenWeatherMap
 ** return:	weather data from OpenWeatherMap
 */
-function get_weather ($agent, $appid)
+function get_weather($agent, $appid)
 {
-	// Get remote IP address and return if it can't be geolocalized
-	$ip_address = $_SERVER['REMOTE_ADDR'];
-	$is_public = filter_var ($ip_address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false;
+    // Get remote IP address and return if it can't be geolocalized
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+    $is_public = filter_var($ip_address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false;
 
-	if (!$is_public)
-		return null;
+    if (!$is_public) {
+        return null;
+    }
 
-	// Call FreeGeoIP to resolve latitude and longitude and return on invalid response
-	// See: https://freegeoip.app/
-	$geo = http_get_json ($agent, 'https://freegeoip.app/json/' . $ip_address);
+    // Call FreeGeoIP to resolve latitude and longitude and return on invalid response
+    // See: https://freegeoip.app/
+    $geo = http_get_json($agent, 'https://freegeoip.app/json/' . $ip_address);
 
-	if ($geo === null || !isset ($geo['latitude']) || !isset ($geo['longitude']))
-		return null;
+    if ($geo === null || !isset($geo['latitude']) || !isset($geo['longitude'])) {
+        return null;
+    }
 
-	// Call OpenWeatherMap API to get weather information
-	// See: http://openweathermap.org/current#geo
-	$weather = http_get_json ($agent, 'http://api.openweathermap.org/data/2.5/weather?appid=' . rawurlencode ($appid) . '&lat=' . rawurlencode ($geo['latitude']) . '&lon=' . rawurlencode ($geo['longitude']));
+    // Call OpenWeatherMap API to get weather information
+    // See: http://openweathermap.org/current#geo
+    $weather = http_get_json($agent, 'http://api.openweathermap.org/data/2.5/weather?appid=' . rawurlencode($appid) . '&lat=' . rawurlencode($geo['latitude']) . '&lon=' . rawurlencode($geo['longitude']));
 
-	return $weather;
+    return $weather;
 }
 
 require 'config.php';
@@ -164,22 +238,20 @@ require 'config.php';
 // Call Boo variant selection method to get current variant name
 $agent = $config['openweathermap.agent'];
 $appid = $config['openweathermap.appid'];
-$time = time ();
-$user = isset ($_GET['user']) ? (int)$_GET['user'] : 0;
+$time = time();
+$user = isset($_GET['user']) ? (int)$_GET['user'] : 0;
 
-$name = get_variant ($agent, $appid, $time, $user);
+$name = get_variant($agent, $appid, $time, $user);
 $path = $config['image.path'] . '/' . $name . $config['image.extension'];
 
 // Emit HTTP redirection with caching directives
-header ('Cache-Control: public, max-age=' . $config['image.expire']);
-header ('Content-Length: ' . filesize ($path));
-header ('Content-Type: ' . $config['image.mime']);
-header ('Expires: ' . gmdate ('D, d M Y H:i:s', $time + $config['image.expire']) . ' GMT');
+header('Cache-Control: public, max-age=' . $config['image.expire']);
+header('Content-Length: ' . filesize($path));
+header('Content-Type: ' . $config['image.mime']);
+header('Expires: ' . gmdate('D, d M Y H:i:s', $time + $config['image.expire']) . ' GMT');
 
 // Store name in cookie so it can be read from JavaScript
-setcookie ('boo', $name, $time + $config['image.expire'] + 1, $config['cookie.path'], $config['cookie.domain']);
+setcookie('boo', $name, $time + $config['image.expire'] + 1, $config['cookie.path'], $config['cookie.domain']);
 
 // Read image contents
-readfile ($path);
-
-?>
+readfile($path);
--- static/animate.js
+++ static/animate.js
@@ -1,63 +1,56 @@
 /*
 ** Display selected Boo variant, start associated animation if any, and handle jingles.
 */
-$(function ()
-{
-	var createButton = function (basePath, name)
-	{
+$(function () {
+	var createButton = function (basePath, name) {
 		return $('<img style="position: absolute; left: 0px; top: 0px; opacity: 0.75; width: 20px; height: 20px;">')
-			.prop ('src', basePath + '/ui/' + name + '.png');
+			.prop('src', basePath + '/ui/' + name + '.png');
 	};
 
-	var createLayer = function (basePath, name)
-	{
+	var createLayer = function (basePath, name) {
 		return $('<img class="default-mascot" style="position: absolute; left: 0; top: 0; opacity: 0;">')
-			.prop ('src', basePath + '/image/' + name + '.png');
+			.prop('src', basePath + '/image/' + name + '.png');
 	}
 
 	/*
 	** Load and play a jingle from a URL
 	** jingleURL:		URL of jingle to play
 	*/
-	var jinglePlay = function (basePath, name)
-	{
+	var jinglePlay = function (basePath, name) {
 		// Load the data
-		$.getJSON (basePath + '/jingle/' + name + '.json', function (jingleData)
-		{
+		$.getJSON(basePath + '/jingle/' + name + '.json', function (jingleData) {
 			// Create the audio context
 			var audioContext;
 
 			if (window.AudioContext !== undefined)
-				audioContext = new window.AudioContext ();
+				audioContext = new window.AudioContext();
 			else if (window.webkitAudioContext !== undefined)
-				audioContext = new window.webkitAudioContext ();
+				audioContext = new window.webkitAudioContext();
 			else
 				return; // Stop if Web Audio API is not supported
 
 			// Set the sound synthesis parameters
 			var duration = 6.0;
-			var frequency = 440.0 * Math.pow (2.0, 15 / 12.0);
+			var frequency = 440.0 * Math.pow(2.0, 15 / 12.0);
 			var harmonics = [[1.0, -13.0, 13.5], [3.0, -40.5, 49.9], [5.5, -2.0, 47.3], [6.0, -25.5, 23.5], [8.0, -31.9, 30.4], [8.5, -11.8, 54.5]];
 			var fadeInDuration = 0.0001;
 			var fadeOutDuration = 1.0;
 
 			// Create an audio buffer for the base note
-			var audioBuffer = audioContext.createBuffer (1, audioContext.sampleRate * duration, audioContext.sampleRate);
-			var audioData = audioBuffer.getChannelData (0);
+			var audioBuffer = audioContext.createBuffer(1, audioContext.sampleRate * duration, audioContext.sampleRate);
+			var audioData = audioBuffer.getChannelData(0);
 
 			// Synthesize the base note
 			// Generate the waveform
-			for (var i = 0; i < audioBuffer.length; i++)
-			{
+			for (var i = 0; i < audioBuffer.length; i++) {
 				var t = i / audioBuffer.sampleRate;
 				var omega = 2.0 * Math.PI * t;
 				var sample = 0.0;
 
-				for (var j = 0; j < harmonics.length; j++)
-				{
-					var amplitude = Math.pow (10, (harmonics[j][1] - (t * harmonics[j][2])) / 20);
+				for (var j = 0; j < harmonics.length; j++) {
+					var amplitude = Math.pow(10, (harmonics[j][1] - (t * harmonics[j][2])) / 20);
 
-					sample += amplitude * Math.sin (omega * frequency * harmonics[j][0]);
+					sample += amplitude * Math.sin(omega * frequency * harmonics[j][0]);
 				}
 
 				audioData[i] = sample;
@@ -78,14 +71,13 @@ $(function ()
 			// Generate the melody using the base note
 			var t = audioContext.currentTime + 0.1;
 
-			for (var i = 0; i < jingleData.melody.length; i++)
-			{
-				var audioSource = audioContext.createBufferSource ();
+			for (var i = 0; i < jingleData.melody.length; i++) {
+				var audioSource = audioContext.createBufferSource();
 
 				audioSource.buffer = audioBuffer;
-				audioSource.playbackRate.value = Math.pow (2.0, jingleData.melody[i][0] / 12.0);
-				audioSource.connect (audioContext.destination);
-				audioSource[audioSource.start ? 'start' : 'noteOn'] (t);
+				audioSource.playbackRate.value = Math.pow(2.0, jingleData.melody[i][0] / 12.0);
+				audioSource.connect(audioContext.destination);
+				audioSource[audioSource.start ? 'start' : 'noteOn'](t);
 
 				t += jingleData.melody[i][1] / 1000.0;
 			}
@@ -96,8 +88,7 @@ $(function ()
 	** Start animation matching name read from "boo" cookie. This method is intended to be called
 	** once request on dynamic image has been completed and set a cookie in return.
 	*/
-	var animate = function ()
-	{
+	var animate = function () {
 		// If image is not loaded yet it means we don't have any cookie value and don't know which
 		// Boo variant has been selected. Let's wait for this information to be available.
 		if (!this.complete)
@@ -108,7 +99,7 @@ $(function ()
 		var target = $(this);
 
 		// Extract base path from script URL
-		var match =  /^(.*)\/animate\.js([?#].*)?$/.exec ($('script[src*="/animate.js"]').prop ('src'));
+		var match = /^(.*)\/animate\.js([?#].*)?$/.exec($('script[src*="/animate.js"]').prop('src'));
 
 		if (!match)
 			return;
@@ -116,55 +107,48 @@ $(function ()
 		var basePath = match[1];
 
 		// Start animation if one is defined for current Boo variant
-		var variant = $.cookie ('boo');
+		var variant = $.cookie('boo');
 
-		switch (variant)
-		{
+		switch (variant) {
 			case 'christmas':
 			case 'christmas-dark':
-				var tick = function (target, duration, steps, index)
-				{
+				var tick = function (target, duration, steps, index) {
 					var delta = 0.25;
-					var pause = steps[index] * (delta * (Math.random () * 2 - 1) + 1);
-					var speed = duration * (delta * (Math.random () * 2 - 1) + 1);
+					var pause = steps[index] * (delta * (Math.random() * 2 - 1) + 1);
+					var speed = duration * (delta * (Math.random() * 2 - 1) + 1);
 
-					target.animate ({opacity: 1 - index % 2}, speed, 'swing', function ()
-					{
-						setTimeout (function ()
-						{
-							tick (target, duration, steps, (index + 1) % steps.length);
+					target.animate({ opacity: 1 - index % 2 }, speed, 'swing', function () {
+						setTimeout(function () {
+							tick(target, duration, steps, (index + 1) % steps.length);
 						}, pause);
 					});
 				};
 
-				wrap (target)
-					.append (createLayer (basePath, 'christmas-glow').addClass ('g'))
-					.append (createLayer (basePath, 'christmas-light0').addClass ('l'))
-					.append (createLayer (basePath, 'christmas-light1').addClass ('l'))
-					.append (createLayer (basePath, 'christmas-light2').addClass ('l'))
-					.append (createLayer (basePath, 'christmas-light3').addClass ('l'))
-					.find ('.g').each (function () { tick ($(this), 1000, [500, 500], 0); }).end ()
-					.find ('.l').each (function () { tick ($(this), 250, [100, 100, 200, 200, 500, 500], 0); }).end ();
+				wrap(target)
+					.append(createLayer(basePath, 'christmas-glow').addClass('g'))
+					.append(createLayer(basePath, 'christmas-light0').addClass('l'))
+					.append(createLayer(basePath, 'christmas-light1').addClass('l'))
+					.append(createLayer(basePath, 'christmas-light2').addClass('l'))
+					.append(createLayer(basePath, 'christmas-light3').addClass('l'))
+					.find('.g').each(function () { tick($(this), 1000, [500, 500], 0); }).end()
+					.find('.l').each(function () { tick($(this), 250, [100, 100, 200, 200, 500, 500], 0); }).end();
 
 				break;
 
 			case 'sleep':
-				var tick = function (target, index)
-				{
-					target.find ('.z' + (index % 3)).animate ({opacity: 1 - Math.floor (index / 3)}, 250, 'linear', function ()
-					{
-						setTimeout (function ()
-						{
-							tick (target, (index + 1) % 6);
+				var tick = function (target, index) {
+					target.find('.z' + (index % 3)).animate({ opacity: 1 - Math.floor(index / 3) }, 250, 'linear', function () {
+						setTimeout(function () {
+							tick(target, (index + 1) % 6);
 						}, 500);
 					});
 				};
 
-				wrap (target)
-					.append (createLayer (basePath, 'sleep-z0').addClass ('z0'))
-					.append (createLayer (basePath, 'sleep-z1').addClass ('z1'))
-					.append (createLayer (basePath, 'sleep-z2').addClass ('z2'))
-					.each (function () { tick ($(this), 0); });
+				wrap(target)
+					.append(createLayer(basePath, 'sleep-z0').addClass('z0'))
+					.append(createLayer(basePath, 'sleep-z1').addClass('z1'))
+					.append(createLayer(basePath, 'sleep-z2').addClass('z2'))
+					.each(function () { tick($(this), 0); });
 
 				break;
 
@@ -184,16 +168,15 @@ $(function ()
 			case 'totoro':
 			case 'unicorn':
 			case 'yoshi':
-				wrap (target)
-					.append (createButton (basePath, 'play').on ('click', function ()
-					{
-						jinglePlay (basePath, variant);
+				wrap(target)
+					.append(createButton(basePath, 'play').on('click', function () {
+						jinglePlay(basePath, variant);
 					}));
 
 				break;
 
 			default:
-				wrap (target, true);
+				wrap(target, true);
 
 				break;
 		}
@@ -207,21 +190,19 @@ $(function ()
 	**				wrap otherwise
 	** return:		wrapper element
 	*/
-	var wrap = function (target, removeOnly)
-	{
-		var parent = target.parent ('.animate');
+	var wrap = function (target, removeOnly) {
+		var parent = target.parent('.animate');
 
 		// Create parent and wrap target element when missing
-		if (parent.length === 0)
-		{
+		if (parent.length === 0) {
 			if (removeOnly)
 				return target;
 
-			return target.wrap ($('<div class="animate" style="position: relative;">')).parent ();
+			return target.wrap($('<div class="animate" style="position: relative;">')).parent();
 		}
 
 		// Otherwise remove all child elements but original target
-		parent.children ().not (target).remove ();
+		parent.children().not(target).remove();
 
 		return parent;
 	};
@@ -229,7 +210,7 @@ $(function ()
 	// Bind animation callback to image load event + trigger manually once in case image was
 	// already loaded from cache at this point. This implies "animate" may be called twice.
 	$('#yn-mascot')
-		.prop ('alt', 'Boo')
-		.one ('load', animate)
-		.each (animate);
+		.prop('alt', 'Boo')
+		.one('load', animate)
+		.each(animate);
 });
